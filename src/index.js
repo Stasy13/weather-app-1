005eae8f1c4249f470b6ba8c8acb2846b3c278d1
Stasy13 <nyshka02@gmail.com>
2023-06-18 10:51:35 +0100

    changed city name js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 let apiKey = "oe4a80734b0b63307f116671db02tf0c";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=Broad Oak&key=oe4a80734b0b63307f116671db02tf0c&units=metric";
-console.log(apiUrl);
+  "https://api.shecodes.io/weather/v1/current?query=BroadOak&key=oe4a80734b0b63307f116671db02tf0c&units=metric";
