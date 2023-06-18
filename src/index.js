a6ad77d3408b9541d0d9da5c7c6490872d870b72
Stasy13 <nyshka02@gmail.com>
2023-06-18 15:13:51 +0100

    Created api -functions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(responce) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+
+  temperatureElement.innerHTML = Math.round(responce.data.temperature.current);
+  cityElement.innerHTML = responce.data.city;
+  descriptionElement.innerHTML = responce.data.condition.description;
+  humidityElement.innerHTML = responce.data.temperature.humidity;
+  windElement.innerHTML = Math.round(responce.data.wind.speed);
+  dateElement.innerHTML = formatDate(responce.data.time * 1000);
+}
+
 let apiKey = "oe4a80734b0b63307f116671db02tf0c";
+let city = "London";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=BroadOak&key=oe4a80734b0b63307f116671db02tf0c&units=metric";
+  "https://api.shecodes.io/weather/v1/current?query=${city}&key=oe4a80734b0b63307f116671db02tf0c&units=metric";
+
+axios.get(apiUrl).then(dasplayTemperature);
