7a3e748e3bc14b9859366ce10bd1da7fcf4fae6c
Stasy13 <nyshka02@gmail.com>
2023-06-18 10:46:18 +0100

    created api

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let apiKey = "oe4a80734b0b63307f116671db02tf0c";
+let apiUrl =
+  "https://api.shecodes.io/weather/v1/current?query=Broad Oak&key=oe4a80734b0b63307f116671db02tf0c&units=metric";
+console.log(apiUrl);
