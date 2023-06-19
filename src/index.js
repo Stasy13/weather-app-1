c45cc228f7db48fa08fac7997c1ed2c646da9f87
Stasy13 <nyshka02@gmail.com>
2023-06-19 19:28:34 +0100

    changed mistake

--- src/index.js
+++ src/index.js
@@ -41,4 +41,4 @@ let apiKey = "oe4a80734b0b63307f116671db02tf0c";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=Hastings&key=oe4a80734b0b63307f116671db02tf0c&units=metric";
 
-axios.get(apiUrl).then(dasplayTemperature);
+axios.get(apiUrl).then(displayTemperature);
